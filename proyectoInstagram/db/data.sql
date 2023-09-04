79d044a1a28057ad0d4c5ff237972dc77f813534
micaelasc <mschachtl@udesa.edu.ar>
2023-09-04 17:33:23 -0300

    inserto tabla usuarios

--- proyectoInstagram/db/data.sql
+++ proyectoInstagram/db/data.sql
@@ -6,6 +6,7 @@ USE TPI_PROGRAII;
 CREATE TABLE usuario(
 /* nombreColumna				tipoDato			restricciones/connstraints (unsigned=numeros positivos)*/
 idUsuario						INT					UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+arroba							VARCHAR(50)			NOT NULL,
 pass							VARCHAR(45)			NOT NULL,
 fotoDePerfil					VARCHAR(200)		NOT NULL,
 fecha							TIMESTAMP			NOT NULL,
@@ -57,3 +58,15 @@ createdAt						TIMESTAMP			DEFAULT CURRENT_TIMESTAMP,
 updatedAt						TIMESTAMP			DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 deletedAt						TIMESTAMP			NULL
 );
+
+/* DATOS PARA TABLA USUARIOS*/
+
+INSERT INTO usuario (idUsuario, arroba, pass, fotoDePerfil, fecha, dni, createdAt, updatedAt, deletedAt) VALUES (DEFAULT, "micaschachtl", 2502,  "",  "2003-02-25 00:00:00", 44713206, DEFAULT, DEFAULT, DEFAULT); 
+INSERT INTO usuario (idUsuario, arroba, pass, fotoDePerfil, fecha, dni, createdAt, updatedAt, deletedAt) VALUES (DEFAULT, "valitaquispe", 1204,  "", "2004-04-12 12:00:00", 45547793, DEFAULT, DEFAULT, DEFAULT); 
+INSERT INTO usuario (idUsuario, arroba, pass, fotoDePerfil, fecha, dni, createdAt, updatedAt, deletedAt) VALUES (DEFAULT, "valenflores", 3333,  "", "2002-11-08 07:00:00", 44486249, DEFAULT, DEFAULT, DEFAULT); 
+INSERT INTO usuario (idUsuario, arroba, pass, fotoDePerfil, fecha, dni, createdAt, updatedAt, deletedAt) VALUES (DEFAULT,  "jazquispe", 0131, "", "2001-01-31 16:00:00", 43640097, DEFAULT, DEFAULT, DEFAULT); 
+INSERT INTO usuario (idUsuario, arroba, pass, fotoDePerfil, fecha, dni, createdAt, updatedAt, deletedAt) VALUES (DEFAULT,  "mdipace", 8008,  "", "2003-07-18 08:03:00", 45017261, DEFAULT, DEFAULT, DEFAULT); 
+
+
+
+
