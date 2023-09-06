65e41262f84491dcbd6cf5a6f03b31a8449edcd4
ValentinaQuispe12 <vquispe@udesa.edu.ar>
2023-09-06 12:41:54 -0300

    creacion de formularios de register a login y de login a register

--- proyectoInstagram/db/data.js
+++ proyectoInstagram/db/data.js
@@ -1,8 +1,8 @@
 /*OBJETO LITERAL*/
 
-const grama = {
+/* const grama = {
     usuarios: [
-        {id: 1,     arroba: micaschachtl,   pass: 2502,     fecha: "2003-02-25 00:00:00",   dni : 44713206,	    createdAt : "2023-09-04 17:03:07", 	updatedAt : "2023-09-04 17:03:07"},	
+        {id: 1,     arroba: micaschachtl,   pass: 2502,     fecha: "2003-02-25 00:00:00",   dni : 44713206,	    createdAt : "2023-09-04 17:03:07", 	updatedAt : "2023-09-04 17:03:07"},
         {id: 2,     arroba: valitaquispe,   pass: 1204,     fecha: "2004-04-12 12:00:00",   dni : 45547793,	    createdAt :	"2023-09-04 17:03:07", 	updatedAt :	"2023-09-04 17:03:07"},	
         {id: 3,	    arroba: valenflores,    pass: 3333,     fecha: "2002-11-08 07:00:00",   dni : 44486249,	    createdAt :"2023-09-04 17:03:07", 	updatedAt :"2023-09-04 17:03:07"},	
         {id:4,	    arroba: jazquispe,      pass: 131,      fecha: "2001-01-31 16:00:00",   dni : 43640097,	    createdAt :"2023-09-04 17:03:07", 	updatedAt :"2023-09-04 17:03:07"},	
@@ -10,4 +10,4 @@ const grama = {
     posteos:{},
     comentarios:{}
 };
-module.export = grama;
+module.export = grama; */
